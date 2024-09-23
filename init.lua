961875f4d5124eb0350a846886a5ce947c3d4ac4
Thiago  Meller Casagrande <thiagomellercasagrande@gmail.com>
2024-09-23 11:26:53 -0300

    nvimtree

--- init.lua
+++ init.lua
@@ -27,6 +27,33 @@ require("lazy").setup({
   { import = "plugins" },
 }, lazy_config)
 
+require("nvim-tree").setup({
+  update_cwd = true,
+  actions = {
+  open_file = {
+  resize_window = true,
+  },
+  },
+  view = {
+  side = "left",
+  },
+  update_focused_file = {
+  enable = true,
+  update_cwd = true,
+  },
+  filters = {
+  dotfiles = false,
+  },
+  diagnostics = {
+  enable = true,
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 500,
+  },
+  })
+
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
